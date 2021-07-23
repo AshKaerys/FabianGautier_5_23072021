3d29b06c63641bcfbe1814bfdf2295028ff2e6b5
Ash <ashkaerys@gmail.com>
2021-07-23 16:38:09 +0200

    Add index

---
+++ index.js
@@ -0,0 +1,10 @@
+// fetch api
+
+fetch("http://localhost:3000/api/teddies")
+    .then((res) => console.log(res));
+
+fetch("http://localhost:3000/api/cameras")
+    .then((res) => console.log(res));
+
+fetch("http://localhost:3000/api/furniture")
+    .then((res) => console.log(res));

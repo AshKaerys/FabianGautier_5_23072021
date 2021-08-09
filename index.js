dbc701477967cfb876b81a673c2364acd01ce074
Ash <ashkaerys@gmail.com>
2021-08-09 15:53:24 +0200

    Ajout début de  javascript index et page produit

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 // fetch api
 
-fetch("http://localhost:3000/api/teddies")
-    .then((res) => console.log(res));
-
 fetch("http://localhost:3000/api/cameras")
-    .then((res) => console.log(res));
-
+    //.then((res) => console.log(res));
 fetch("http://localhost:3000/api/furniture")
-    .then((res) => console.log(res));
+    //.then((res) => console.log(res));
 
-const product = document.getElementsByClassName("product");
 const navBar = document.querySelector("nav");
+const product = document.getElementsByClassName("product");
+const select = document.getElementById("select");
+let bear = document.getElementById("bear");
 
-window.addEventListener("scroll", (e) => {
-    console.log(e);
+window.addEventListener("scroll", () => {
     if (window.scrollY > 50){
         navBar.style.top = "-115px";
     }
@@ -21,3 +18,41 @@ window.addEventListener("scroll", (e) => {
         navBar.style.top = "0px";
     }
 });
+
+ // récupération API
+
+fetch("http://localhost:3000/api/teddies")
+     .then((res) => res.json())
+     .then((data) => console.log(data));
+
+fetch("http://localhost:3000/api/teddies")
+    .then((res) => res.json())
+    .then(function (data){
+        let teddies = data
+        return teddies.map(function(teddy){
+            let div = document.createElement('div')
+            div.classList.add('product')
+            div.innerHTML=`
+            <a href="./product.html?id=${teddy._id}">
+            <h2>${teddy.name}</h2>
+            <img src="${teddy.imageUrl}" alt="Ours en peluche">
+            </a>
+            `
+            bear.appendChild(div)
+        })
+    })
+    .catch(function(error){console.log(error)});
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+
---
+++ product.js
@@ -0,0 +1,47 @@
+
+const navBar = document.querySelector("nav");
+const product = document.getElementsByClassName("product");
+const select = document.getElementById("select");
+let bear = document.getElementById("bear");
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50){
+        navBar.style.top = "-115px";
+    }
+    else {
+        navBar.style.top = "0px";
+    }
+});
+
+ // récupération API
+
+ fetch("http://localhost:3000/api/teddies")
+    .then((res) => res.json())
+    //.then((data) => console.log(data));
+
+
+
+// chargement url by ID
+
+let url = new URL(document.location).searchParams;
+let id = url.get('id');
+console.log(id);
+
+fetch (`http://localhost:3000/api/teddies/${id}`)
+    .then ((res) => res.json())
+    //.then ((data) => console.log(data))
+    .then (function (data){
+        let teddy = data
+        console.log(teddy);
+        let div = document.createElement('div')
+        div.classList.add('product')
+        div.innerHTML=`
+        <h1>${teddy.name}</h1>
+        <img src="${teddy.imageUrl}" alt="image de l'ours ${teddy.name}">
+        <p>${teddy.description}</p>
+        <p>${teddy.price}€<p>
+        `
+        bear.appendChild(div)
+        
+    })
